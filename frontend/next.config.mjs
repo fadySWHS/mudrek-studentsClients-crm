3fcea449bcb1a9269dbc5f39e4d5d89e26fde22c
Mudrek Dev <mudrek@dev.local>
2026-03-30 23:03:18 +0200

    feat: add dockerization with docker-compose and node 20 dockerfiles

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://api.studentsclients.mudrek.com/api',
   },
 };
 
